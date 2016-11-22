8c158ba82959d7a1a42c1ee2f9a73527d9d904a8
mbelgrader <mbelgrader5@gmail.com>
2016-11-22 12:02:37 -0800

    Add stepping piece logic

--- piece.rb
+++ piece.rb
@@ -23,46 +23,31 @@ end
 
 class King < Piece
   include SteppingPiece
-  # POSITIONS = [
-  #     [- 1, - 1],
-  #     [- 1, 0],
-  #     [- 1, 1],
-  #     [0, - 1],
-  #     [0, 1],
-  #     [1, - 1],
-  #     [1, 0],
-  #     [1, 1]
-  #   ]
 
-
+  def move_dirs
+    horizontal + diagonals
+  end
 end
 
 class Knight < Piece
   include SteppingPiece
-  # POSITIONS = [
-  #     [- 2, 1],
-  #     [- 1, 2],
-  #     [1, 2],
-  #     [2, 1],
-  #     [2, - 1],
-  #     [1, - 2],
-  #     [- 1, - 2],
-  #     [- 2, - 1]
-  #   ]
+
+  def move_dirs
+    knight
+  end
 end
 
 class Queen < Piece
   include SlidingPiece
 
-    def move_dirs
-      horizontal + diagonals
-    end
-
-
+  def move_dirs
+    horizontal + diagonals
+  end
 end
 
 class Bishop < Piece
   include SlidingPiece
+
   def move_dirs
     diagonals
   end
@@ -70,10 +55,35 @@ end
 
 class Rook < Piece
   include SlidingPiece
+
   def move_dirs
     horizontal
   end
 end
 
 class Pawn < Piece
+  include SteppingPiece
+
+  def moves
+    result = []
+    move_dirs.each do |dir|
+      postion = add_pos(pos, dir)
+      result << postion if valid_move?(postion) || attack?(postion)
+    end
+
+    result
+  end
+
+  def attack?(position)
+    !board[position].is_a?(NullPiece) && board[position].color != color
+  end
+
+  def move_dirs
+    if color == black
+      [[1, 0], [1, -1], [1, 1]]
+    else
+      [[-1, 0], [-1, -1], [-1, 1]]
+    end
+  end
+
 end
--- stepping_piece.rb
+++ stepping_piece.rb
@@ -1,3 +1,52 @@
 module SteppingPiece
 
+  def moves
+    result = []
+    move_dirs.each do |dir|
+      position = add_pos(@pos, dir)
+      result << position if valid_move?(position)
+    end
+
+    result
+  end
+
+  def add_pos(prev, dir)
+    [prev[0] + dir[0], prev[1] + dir[1]]
+  end
+
+  def valid_move?(move)
+    @board.in_bounds?(move) &&
+    (@board[move].is_a?(NullPiece) || @board[move].color != @color)
+  end
+
+  def horizontal
+    [
+      [- 1, 0],
+      [0, - 1],
+      [0, 1],
+      [1, 0]
+    ]
+  end
+
+  def diagonals
+    [
+      [- 1, - 1],
+      [- 1, 1],
+      [1, - 1],
+      [1, 1]
+    ]
+  end
+
+  def Knight
+    [
+      [- 2, 1],
+      [- 1, 2],
+      [1, 2],
+      [2, 1],
+      [2, - 1],
+      [1, - 2],
+      [- 1, - 2],
+      [- 2, - 1]
+     ]
+   end
 end
